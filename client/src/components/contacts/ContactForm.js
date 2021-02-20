babefe5ac07b9159ab8da53aa970a705b2d30680
irisida <20445648+irisida@users.noreply.github.com>
2021-02-20 18:31:01 +0000

    add new contact via the UI, to the UI and state via context, presistence not added as yet

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,85 @@
+import React, { useState, useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+const ContactForm = () => {
+  const contactContext = useContext(ContactContext);
+
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    type: 'personal',
+  });
+
+  const { name, email, phone, type } = contact;
+
+  const onChange = (e) =>
+    setContact({
+      ...contact,
+      [e.target.name]: e.target.value,
+    });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    contactContext.addContact(contact);
+    setContact({
+      name: '',
+      email: '',
+      phone: '',
+      type: 'personal',
+    });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h2 className='text-primary'>Add Contact</h2>
+      <input
+        type='text'
+        placeholder='name'
+        name='name'
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        type='text'
+        placeholder='email'
+        name='email'
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type='text'
+        placeholder='phone'
+        name='phone'
+        value={phone}
+        onChange={onChange}
+      />
+      <h5>Contact Type</h5>
+      <input
+        type='radio'
+        name='type'
+        value='personal'
+        onChange={onChange}
+        checked={type === 'personal' ? true : false}
+      />{' '}
+      Personal{' '}
+      <input
+        type='radio'
+        name='type'
+        value='professional'
+        onChange={onChange}
+        checked={type === 'professional' ? true : false}
+      />{' '}
+      Professional
+      <div>
+        <input
+          className='btn btn-primary btn-block'
+          type='submit'
+          value='Add Contact'
+        />
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
