65020447f481d267aafa78a53acfb30df2924171
irisida <20445648+irisida@users.noreply.github.com>
2021-02-21 13:56:19 +0000

    added login and register forms and routes

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -24,3 +24,23 @@ export const CLEAR_CURRENT = 'CLEAR_CURRENT';
  */
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+/**
+ * auth types for registration and load
+ */
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+
+/**
+ * auth types for login
+ */
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+
+/**
+ * auth types for error
+ */
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const AUTH_ERROR = 'AUTH_ERROR';
